fbf97c03c4b112a0d21c272e09df119c078719d7
KumarManish12 <mkb.42700@gmail.com>
2023-02-28 16:17:38 +0530

    activity
    activity lifecucle

--- app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragmentlifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentContainer
 import androidx.fragment.app.FragmentManager
 import com.example.fragmentlifecycle.databinding.ActivityMainBinding
@@ -13,6 +14,43 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Toast.makeText(this,"this is ONCreate",Toast.LENGTH_LONG).show()
+
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"This is ONRestart",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"This is ONStop",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"This is ONPause",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"This is ONDestroy",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"This is ONResume",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"This is ONStart",Toast.LENGTH_LONG).show()
+
+    }
 }
